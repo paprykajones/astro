13196f40f8551d711871f64a15b013c1e3659c2d
meegosh <mee>
2023-04-28 18:27:25 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // GitLab Pages requires exposed files to be located in a folder called "public".
+  // So we're instructing Astro to put the static build output in a folder of that name.
+  outDir: 'public',
+
+  // The folder name Astro uses for static files (`public`) is already reserved
+  // for the build output. So in deviation from the defaults we're using a folder
+  // called `static` instead.
+  publicDir: 'static',
+});
