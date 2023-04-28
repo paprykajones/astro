625080e7c577b63574a7e955443cf349e133275d
meegosh <mee>
2023-04-28 18:29:46 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,42 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // GitLab Pages requires exposed files to be located in a folder called "public".
-  // So we're instructing Astro to put the static build output in a folder of that name.
-  outDir: 'public',
-
-  // The folder name Astro uses for static files (`public`) is already reserved
-  // for the build output. So in deviation from the defaults we're using a folder
-  // called `static` instead.
-  publicDir: 'static',
+  site: SITE.website,
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
